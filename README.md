53d903705fd9c74a205395a5c8d5eb79515b96da
Marwan Alani <marwan@alani.ca>
2017-06-26 11:31:04 -0400

    Wrote the solution for the challenge. Added a README file

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# code_for_canada_challenge
-Working on a coding challenge for FellowShip Canada
+# Code for Canada - Developers' Challenge
+
+Attached is my solution for the coding challenge received as part of my application to the "Code for Canada" FellowShip.  
+
+My solution is written in _Swift_, and took me about 30 minutes to write.  
+
+-----
+### Requirements  
+- Swift 3 compiler (Installed separately on Linux. Comes embedded with XCode on Mac OS)
+- Mac OS 10.11+ (or Ubuntu Linux 14.04+)
+- All files of this repo to be placed in the same directory
+
+### Usage
+Open your terminal, go to the root of the directory where you copied this repo, then run the following command:  
+`swift Solution.swift`
+
+-----
+
+Marwan Alani - 2017
---
+++ Solution.swift
@@ -0,0 +1,55 @@
+//
+//  Solution.swift
+//
+//  Created by Marwan Alani on 2017-06-26
+//  Copyright © 2017 Marwan Alani. All rights reserved.
+//
+
+import Foundation
+
+// Define a data structure to hold required info for a single violation category
+struct Stats {
+  var count: Int
+  var earliestOccurance: Date
+  var latestOccurance: Date
+}
+
+// Declare global variables
+var output = [String : Stats]()
+let inputPath = FileManager.default.currentDirectoryPath + "/dev-challenge-data.csv"
+let calendar = Calendar.current
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd"
+
+// Attempt to open the CSV file and process the violations, one line at a time
+do {
+  let input = try String(contentsOfFile: inputPath).components(separatedBy: "\n")
+  for lineNumber in 1..<input.count {
+    let fields = input[lineNumber].components(separatedBy: ",")
+    let dateArray = fields[3].components(separatedBy: " ")[0].components(separatedBy: "-").map({Int($0)!})
+    let dateComponents = DateComponents(calendar: calendar, year: dateArray[0], month: dateArray[1], day: dateArray[2])
+    let dateOfViolation = calendar.date(from: dateComponents)!
+    if (output[fields[2]] != nil) {
+      output[fields[2]]!.count += 1
+      if (output[fields[2]]!.earliestOccurance > dateOfViolation) {
+        output[fields[2]]!.earliestOccurance = dateOfViolation
+      } else if (output[fields[2]]!.latestOccurance < dateOfViolation) {
+        output[fields[2]]!.latestOccurance = dateOfViolation
+      }
+    } else {
+      output[fields[2]] = Stats(count: 1, earliestOccurance: dateOfViolation, latestOccurance: dateOfViolation)
+    }
+  }
+} catch {
+  print("Sorry, something went wrong and we couldn't process your request")
+}
+
+// Print the output
+print("\n<<<<< Beginning of Report >>>>>\n")
+for violation in output {
+  print("Category: \(violation.key)")
+  print("\tNumber of Violations: \(violation.value.count)")
+  print("\tEarliest Violation: \(dateFormatter.string(from: violation.value.earliestOccurance))")
+  print("\tLatest Violation: \(dateFormatter.string(from: violation.value.latestOccurance))\n")
+}
+print("\n<<<<< End of Report >>>>>\n")
